9c4f8ac1f06f8414679a90a24806264699eb62d6
Marta Kalinowska <martka88@gmail.com>
2020-05-31 18:37:11 +0200

    Deleting TO DO comments from analysis part

--- bikeshare.py
+++ bikeshare.py
@@ -79,17 +79,14 @@ def time_stats(df):
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
-    # TO DO: display the most common month
     df['month'] = df['Start Time'].dt.month
     popular_month = df['month'].mode()[0]
     print('Most common month:', popular_month)
 
-    # TO DO: display the most common day of week
     df['weekday'] = df['Start Time'].dt.weekday
     popular_weekday = df['weekday'].mode()[0]
     print('Most common day of the week:', popular_weekday)
 
-    # TO DO: display the most common start hour
     df['hour'] = df['Start Time'].dt.hour
     popular_hour = df['hour'].mode()[0]
     print('Most common start hour:', popular_hour)
